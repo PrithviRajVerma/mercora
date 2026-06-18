f0d89bdcf41989ed73bdeb4a61dbfe648470b23a
PrithviRajVerma <2266prr@gmail.com>
2026-06-18 18:56:25 +0530

    initial project structure

---
+++ auth-service/src/test/java/com/mercora/platform/auth/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercora.platform.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,13 @@
+rootProject.name = "mercora"
+
+include(
+    "common-lib",
+    "api-gateway",
+    "auth-service",
+    "product-service",
+    "cart-service",
+    "order-service",
+    "payment-service",
+    "notification-service",
+    "analytics-service"
+)
